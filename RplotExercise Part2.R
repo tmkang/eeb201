065c02e08c426b0c2cf77f8b462b4555466de6df
tmkang <tmkang@g.ucla.edu>
2015-09-21 01:24:17 -0700

    changes made

--- RplotExercise Part2.R
+++ RplotExercise Part2.R
@@ -7,14 +7,12 @@ setwd("/Users/tina/GitHub/eeb201")
 zzdataframe=read.table("pheno.sim.2014-1.txt", header = TRUE)
 dim(zzdataframe)
 
-bldglucmeas=zzdataframe[,1]
+bldglucmeas=zzdataframe[,2]
 
 length(bldglucmeas)
 
 quantile(bldglucmeas)
 
-contrls=(bldglucmeas<15.75)
-
+controls=c(bldglucmeas<"4.768756")
 length(controls)
-
 
